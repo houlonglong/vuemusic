d04500be78eec6012ca65e440f53dd7caa4365be
houlonglong <31363537@qq.com>
2018-03-01 08:20:32 +0800

    初始化目录结构

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
+//import router from './router'
+import 'common/stylus/index.styl'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
