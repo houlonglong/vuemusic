8605c8598f7bfb80c4b428f011a9cd62cecda3b7
houlonglong <31363537@qq.com>
2018-03-06 21:22:03 +0800

    slider end

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,9 +1,9 @@
-import orignJSONP from 'jsonp'
+import originJsonp from 'jsonp'
 
 export default function jsonp(url,data,option){
-  url = (url.indexOf('?')<0 ? "?":"&") + param(data)  
+  url += (url.indexOf('?')<0 ? "?":"&") + param(data)  
   return new Promise((resolve,reject)=>{
-    orignJSONP(url,option,(err,data)=>{
+    originJsonp(url,option,(err,data)=>{
         if(!err){
             resolve(data)
         }else{
@@ -13,11 +13,11 @@ export default function jsonp(url,data,option){
   })
 }
 
-function param(data){
-    let url = "";
-    for(var k in data){
-        let value = data[k] !== undefined ? data[k] : '';
-        url += `&${k}=${encodeURICompoent(value)}`
+export function param(data) {
+    let url = ''
+    for (var k in data) {
+      let value = data[k] !== undefined ? data[k] : ''
+      url += '&' + k + '=' + encodeURIComponent(value)
     }
     return url ? url.substring(1) : ''
-}
+  }
