0c0343c35eb9c38e16907c227fc7ead2cf0680c1
houlonglong <31363537@qq.com>
2018-03-20 23:40:10 +0800

    左右联动

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="singer">
-          
+          <list-view :data="singers"></list-view>
     </div>
 </template>
 
@@ -8,65 +8,118 @@
 import { getSingerList } from 'api/singer'
 import { ERR_OK } from 'api/config'
 import  Singer  from 'common/js/singer'
+import  ListView  from 'base/listview/listview'
+console.log(ListView)
 const HOT_NAME = '热门'
-const HOT_SINGR_LEN = 10
+const HOT_SINGER_LEN = 10
 export default {
   data(){
      return{
         singers:[]
      }
   },
+  components:{
+      ListView
+  },
   created(){
     this._getSingerList()
   },
+  
   methods:{
       _getSingerList(){
         getSingerList().then((res)=>{
             if(res.code === ERR_OK ){
-                this.singers = res.data.list
-                this.__normlizeSlinger(res.data.list)
+                this.singers = this._normlizeSlinger(res.data.list)
             }
         })
       },
-      __normlizeSlinger(list){
+      _normlizeSlinger(list){
             //热门数据和聚合数据
-            let map={
-                hot:{
-                    title:HOT_NAME,
-                    items:[]
+            let map = {
+                hot: {
+                    title: HOT_NAME,
+                    items: []
                 }
-            }
-            list.forEach((itme,index) => {
-                if(index < HOT_SINGR_LEN){
-                    map.hot.items.push(new Singer(itme.Fsinger_mid,itme.Fsinger_name))
+              }
+                list.forEach((item,index) => {
+                if (index < HOT_SINGER_LEN) {
+                        map.hot.items.push(new Singer({
+                        name: item.Fsinger_name,
+                        id: item.Fsinger_mid
+                      }))
                 }
                 //聚类
-                const key = itme.Findex
+                const key = item.Findex
                 if(!map[key]){
                     map[key] = {
                         title:key,
-                        itmes:[]
+                        items:[]
                     }
                 }
-                map[key].itmes.push(new Singer(itme.Fsinger_mid,itme.Fsinger_name))
+                map[key].items.push(new Singer({
+                    name: item.Fsinger_name,
+                    id: item.Fsinger_mid
+          }))
             });
-            //为了得到有序列表
-            let hot = [],ret = [];
-            for(let key in map){
-                let val = map[key]
-                if(val.title.match(/[a-zA-z]/)){
-                    ret.push(val)
-                }else if(val.title === HOT_NAME){
-                    hot.push(val)
-                }
-            }
-            ret.sort((a,b)=>{
-                return a.title.charCodeAt(0) - b.title.charCodeAt(0)
-            })
-            //console.log([...hot, ...ret] , hot.concat(ret) )
-           return [...hot, ...ret]
+        //为了得到有序列表
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return [...hot,...ret]
          
-      }
+      },
+       _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+        // 为了得到有序列表，我们需要处理 map
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return [...hot,...ret]
+      },
   }
   
 
